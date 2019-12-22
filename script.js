cd04e877ca0487cb4fd7982f7148bd57c47bae68
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-22 16:46:20 -0500

    BUTTON TOGGLE/STATE MANAGEMENT
    - button state managment; no attribute added
    - 25 buttons added
    - progressive web applications links added to README.md

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 console.log("script.js CONNECTED");
 
 function resetBackgroundStyle() {
-    let elementsArray = document.getElementsByTagName('li');
-    
-    for (let i = 0; i < elementsArray.length; i++) {
-        elementsArray[i].style.backgroundColor = 'lightblue';
-    }
-    
-    // elementsArray.forEach(element => {
-    //     element.style.backgroundColor = 'lightblue';
-    // });
-    // console.log(elementsArray);
+  let elementsArray = document.getElementsByTagName("li");
+
+  for (let i = 0; i < elementsArray.length; i++) {
+    elementsArray[i].style.backgroundColor = "lightblue";
+  }
+}
+
+function toggleButtonColor(clickedElement) {
+  // Add a temporary state attribute to clickedElement
+  if (clickedElement.state == true) {
+    clickedElement.state = false;
+  } else {
+    clickedElement.state = true;
+  }
+
+  var buttonState = clickedElement.state;
+  console.log(buttonState);
+
+  if (buttonState) {
+    clickedElement.style.background = "lime";
+  } else {
+    clickedElement.style.background = "red";
+  }
 }

73de36c996d7894d2be36cd375a56265e8518120
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-22 02:00:12 -0500

    reset button working

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 console.log("script.js CONNECTED");
 
-function changeColor () {
-    console.log(this.document);
+function resetBackgroundStyle() {
+    let elementsArray = document.getElementsByTagName('li');
+    
+    for (let i = 0; i < elementsArray.length; i++) {
+        elementsArray[i].style.backgroundColor = 'lightblue';
+    }
+    
+    // elementsArray.forEach(element => {
+    //     element.style.backgroundColor = 'lightblue';
+    // });
+    // console.log(elementsArray);
 }

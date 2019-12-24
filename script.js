d525a9eaedd4bfeca4ef93baec210a7b67a20856
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-23 23:12:24 -0500

    quick save

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function getButtonState (clickedElement) {
 }
 
 function renderButtonColor (buttonState, clickedElement) {
-  // Render Button Color
-  if (buttonState) {
-    clickedElement.style.background = "lime";
-  } else {
-    clickedElement.style.background = "red";
-  }
-
   // Toggle Button State
   if (buttonState == true) {
     clickedElement.setAttribute('buttonState', 0);
   } else {
     clickedElement.setAttribute('buttonState', 1);
+  }
+
+  // Render Button Color
+  if (buttonState) {
+    clickedElement.style.background = "red";
+  } else {
+    clickedElement.style.background = "lime";
   }
 }
 
@@ -26,7 +26,7 @@ function resetBackgroundStyle() {
   let elementsArray = document.getElementsByTagName("li");
 
   for (let i = 0; i < elementsArray.length; i++) {
-    elementsArray[i].setAttribute('buttonState', 1);
+    elementsArray[i].setAttribute('buttonState', 0);
     elementsArray[i].style.backgroundColor = "lightblue";
   }
 }

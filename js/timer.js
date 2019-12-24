67fd7e12489be8677db46a20937f519440fed2ac
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-24 01:39:48 -0500

    Added version number. Added timer test.

--- js/timer.js
+++ js/timer.js
@@ -1,26 +1,35 @@
+const buttonScripts = require('./buttonLogic')
+
 // - <https://momentjs.com/>
 
 // Render Current Time
-// var currentTime = document.getElementById('current-time');
-
-// var counter = 0
+var currentTime = document.getElementById('current-time');
+var counter = 0
 
 var getCurrentTime = () => {
   // console.log('getCurrentTime()');
   currentTime.innerHTML = counter;
   counter++;
+//   button01.setAttribute('buttonState', '1');
+//   buttonScripts.getButtonState(button01);
+  
   // currentTime.innerHTML = moment().local();
   // console.log(moment().local());
+  
+  setTimeout(getCurrentTime,1000)
 };
 
-// setTimeout(console.log('YOLO'), 10000);
-// setInterval(getCurrentTime(), 1000);
+// getCurrentTime();
 
 function startTime() {
   var today = moment.tz("Europe/Copenhagen");
+//   var today = moment.locale();
   document.getElementById("watch2").innerHTML = today.format("HH:mm:ss");
   document.getElementById("watch1").innerHTML = today.format("DD/MM/YYYY");
   var t = setTimeout(startTime, 500);
 }
 
-startTime();
+// startTime();
+
+
+

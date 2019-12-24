53c9d90c55cbb033d00dc06f4e0466b08056af1f
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-24 01:41:48 -0500

    Renamed script.js to buttonLogic.js. Added module.exports (needs testing).

---
+++ js/buttonLogic.js
@@ -0,0 +1,40 @@
+console.log("script.js CONNECTED");
+
+function getButtonState(clickedElement) {
+  let buttonState = Boolean(Number(clickedElement.getAttribute("buttonState")));
+
+  renderButtonColor(buttonState, clickedElement);
+
+  return [buttonState, clickedElement];
+}
+
+function renderButtonColor(buttonState, clickedElement) {
+  // Toggle Button State
+  if (buttonState == false) {
+    clickedElement.setAttribute("buttonState", 1);
+  } else {
+    clickedElement.setAttribute("buttonState", 0);
+  }
+
+  // Render Button Color
+  if (buttonState) {
+    clickedElement.style.background = "red";
+  } else {
+    clickedElement.style.background = "lime";
+  }
+}
+
+function resetBackgroundStyle() {
+  let elementsArray = document.getElementsByTagName("li");
+
+  for (let i = 0; i < elementsArray.length; i++) {
+    elementsArray[i].setAttribute("buttonState", 0);
+    elementsArray[i].style.backgroundColor = "lightblue";
+  }
+}
+
+module.exports = {
+  getButtonState,
+  renderButtonColor,
+  resetBackgroundStyle
+}

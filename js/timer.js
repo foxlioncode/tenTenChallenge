e098e3167ed7721b789ec293eef8575fa1a0b31b
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-28 01:44:41 -0500

    styling added; still experienting with timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,43 @@
+// ====================================================================================================
 function renderTimeDisplay () {
     var currentTime = new Date()
-    document.getElementById('current-time').innerHTML = currentTime;
+    document.getElementById('current-time').innerHTML = 
+        `
+        ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}
+        `;
+    // document.getElementById('current-time').innerHTML = currentTime;
+    // document.getElementById('current-time').innerHTML = currentTime.getTime();
 }
 
-setInterval(renderTimeDisplay,1000);
+// setInterval(renderTimeDisplay,1000);
+
+// ====================================================================================================
+
+
+
+var totalSeconds = 0;
+
+function increaseSeconds () {
+    totalSeconds++
+    document.getElementById('current-time').innerHTML = totalSeconds;
+}
+
+
+
+// secondsDisplay();
+
+var startTimer = () => {
+    var secondsDisplay = setInterval(increaseSeconds(), 1);
+}
+
+function renderTimer() {
+}
+
+function stopTimer () {
+    clearInterval(startTimer);
+}
+
+function resetTimer () {
+}
+
+

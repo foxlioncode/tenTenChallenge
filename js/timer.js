ab1c1aad6cb988b976d8b87e81e5d2ac975d49ef
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-29 01:27:18 -0500

    TIMER REFINEMENT
    - leadingZero working using a a terary condition.
    - Feedback display when user presses 'STOP' and 'RESET' buttons.
    
    TODO
    
    - Chronograph is running slow compared to actual timer.
    - Time is lost because it is not simultaneously incrementing the minutes
    as it zeroes the seconds.
    - Chronograph will count all the way up to 60s before incrementing the
    minute value.
    - Millisecond conversion to seconds problematic.

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,10 @@
 // ====================================================================================================
+var totalMilliSeconds = 0;
 var totalSeconds = 0;
+var totalMinutes = 0;
+var totalHours = 0;
+var leadingZero;
+
 var timerCountUp;
 var alreadyRunning = false;
 
@@ -8,28 +13,54 @@ function renderTimer(item) {
 }
 
 function timerFunction() {
-  totalSeconds++;
-  renderTimer(totalSeconds);
+  totalMilliSeconds++;
+
+  if (totalMilliSeconds == 10) {
+    totalMilliSeconds = 0;
+    totalSeconds++;
+  } else if (totalSeconds == 60) {
+    totalSeconds = 0;
+    totalMinutes++;
+  } else if (totalMinutes == 60) {
+    totalMinutes = 0;
+    totalHours++;
+  }
+
+  renderTimer(
+    `${(leadingZero = (totalHours < 10) ? "0" : "")}${totalHours}h
+     ${(leadingZero = (totalMinutes < 10) ? "0" : "")}${totalMinutes}m
+     ${(leadingZero = (totalSeconds < 10) ? "0" : "")}${totalSeconds}s
+     ${totalMilliSeconds}`
+  );
 }
 
 function startTimer() {
   if (alreadyRunning) {
   } else {
     alreadyRunning = true;
-    timerCountUp = setInterval(timerFunction, 1000);
+    timerCountUp = setInterval(timerFunction, 100);
   }
 }
 
 function stopTimer() {
   alreadyRunning = false;
   clearInterval(timerCountUp);
+  renderTimer("STOP");
+  setTimeout(() => {
+    renderTimer(
+      `${totalHours}h ${totalMinutes}m ${totalSeconds}s ${totalMilliSeconds}`
+    );
+  }, 500);
 }
 
 function resetTimer() {
   stopTimer();
+  totalMilliSeconds = 0;
   totalSeconds = 0;
+  totalMinutes = 0;
+  totalHours = 0;
   renderTimer("RESET");
   setTimeout(() => {
-    renderTimer(totalSeconds);
+    renderTimer(totalMilliSeconds);
   }, 500);
 }

4760357da50f30b29d836f5fc5fdbda2e41bca01
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-12-28 23:57:15 -0500

    basic timer cleaned

--- js/timer.js
+++ js/timer.js
@@ -1,20 +1,11 @@
 // ====================================================================================================
-function renderTimeDisplay() {
-  var currentTime = new Date();
-  document.getElementById("current-time").innerHTML = `
-        ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}
-        `;
-  // document.getElementById('current-time').innerHTML = currentTime;
-  // document.getElementById('current-time').innerHTML = currentTime.getTime();
-}
-
-// setInterval(renderTimeDisplay,1000);
-
-// ====================================================================================================
-
 var totalSeconds = 0;
 var timerCountUp;
 var alreadyRunning = false;
+
+function renderTimer(item) {
+  document.getElementById("current-time").innerHTML = item;
+}
 
 function timerFunction() {
   totalSeconds++;
@@ -30,24 +21,15 @@ function startTimer() {
 }
 
 function stopTimer() {
-  //   renderTimer('stopTimer')
   alreadyRunning = false;
   clearInterval(timerCountUp);
 }
 
 function resetTimer() {
-  renderTimer("resetTimer");
   stopTimer();
   totalSeconds = 0;
-  //   renderTimer('RESET');
   renderTimer("RESET");
   setTimeout(() => {
     renderTimer(totalSeconds);
-  }, 5000);
-
-  //   setTimeout(renderTimer(totalSeconds), 5000);
-}
-
-function renderTimer(item) {
-  document.getElementById("current-time").innerHTML = item;
+  }, 500);
 }
